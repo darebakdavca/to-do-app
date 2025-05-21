af85ed595141db2bc5c382d809032fecc071d8ef
David Brablc <brad14@vse.cz>
2025-05-21 19:55:01 +0200

    Wrap sidebar and tasks components in auth check for improved access control

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
         Tasks
     </x-slot:title>
     <div class="grid w-full grid-cols-[auto_1fr] gap-4 p-5 sm:mx-auto">
-        <x-sidebar :taskLists="$taskLists" :activeTaskList="$activeTaskList"></x-sidebar>
-        <x-tasks :tasks="$tasks" :activeTaskList="$activeTaskList"></x-tasks>
+        @auth
+            <x-sidebar :taskLists="$taskLists" :activeTaskList="$activeTaskList"></x-sidebar>
+            <x-tasks :tasks="$tasks" :activeTaskList="$activeTaskList"></x-tasks>
+        @endauth
     </div>
 </x-layout>
